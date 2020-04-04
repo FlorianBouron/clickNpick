15061a3360cf90f248f2ac6b5f982e20d124e8f8
Florian Bouron <bouron.florian@yahoo.fr>
2020-04-04 18:06:38 +0200

    Edit by hardcode default introduction sentence

--- backend/admin/src/containers/HomePage/index.js
+++ backend/admin/src/containers/HomePage/index.js
@@ -4,29 +4,31 @@
  *
  */
 /* eslint-disable */
-import React, { memo } from 'react';
-import { FormattedMessage } from 'react-intl';
-import { get, upperFirst } from 'lodash';
-import { auth } from 'strapi-helper-plugin';
-import PageTitle from '../../components/PageTitle';
+import React, { memo } from "react";
+import { FormattedMessage } from "react-intl";
+import { get, upperFirst } from "lodash";
+import { auth } from "strapi-helper-plugin";
+import PageTitle from "../../components/PageTitle";
 
-import { Block, Container, P, Wave } from './components';
+import { Block, Container, P, Wave } from "./components";
 
 const HomePage = ({ global: { plugins }, history: { push } }) => {
   const hasAlreadyCreatedContentTypes =
-    get(plugins, ['content-manager', 'leftMenuSections', '0', 'links'], []).filter(
-      contentType => contentType.isDisplayed === true
-    ).length > 1;
+    get(
+      plugins,
+      ["content-manager", "leftMenuSections", "0", "links"],
+      []
+    ).filter((contentType) => contentType.isDisplayed === true).length > 1;
 
   const headerId = hasAlreadyCreatedContentTypes
-    ? 'HomePage.greetings'
-    : 'app.components.HomePage.welcome';
-  const username = get(auth.getUserInfo(), 'username', '');
+    ? "HomePage.greetings"
+    : "app.components.HomePage.welcome";
+  const username = get(auth.getUserInfo(), "username", "");
 
   return (
     <>
       <FormattedMessage id="HomePage.helmet.title">
-        {title => <PageTitle title={title} />}
+        {(title) => <PageTitle title={title} />}
       </FormattedMessage>
       <Container className="container-fluid">
         <div className="row">
@@ -39,37 +41,12 @@ const HomePage = ({ global: { plugins }, history: { push } }) => {
                   name: upperFirst(username),
                 }}
               >
-                {msg => <h2 id="mainHeader">{msg}</h2>}
+                {(msg) => <h2 id="mainHeader">{msg}</h2>}
               </FormattedMessage>
-              {hasAlreadyCreatedContentTypes ? (
-                <FormattedMessage id="app.components.HomePage.welcomeBlock.content.again">
-                  {msg => <P>{msg}</P>}
-                </FormattedMessage>
-              ) : (
-                <FormattedMessage id="HomePage.welcome.congrats">
-                  {congrats => {
-                    return (
-                      <FormattedMessage id="HomePage.welcome.congrats.content">
-                        {content => {
-                          return (
-                            <FormattedMessage id="HomePage.welcome.congrats.content.bold">
-                              {boldContent => {
-                                return (
-                                  <P>
-                                    <b>{congrats}</b>&nbsp;
-                                    {content}&nbsp;
-                                    <b>{boldContent}</b>
-                                  </P>
-                                );
-                              }}
-                            </FormattedMessage>
-                          );
-                        }}
-                      </FormattedMessage>
-                    );
-                  }}
-                </FormattedMessage>
-              )}
+              <FormattedMessage id="app.components.HomePage.welcomeBlock.content.again">
+                {() => <P>Clic & Farm is happy to see you! You'll find in your admin panel all functionalities to support the farmer community. 
+If you need any help, our hotline is open 24/7, at the number +48 111 222 333</P>}
+              </FormattedMessage>
             </Block>
           </div>
         </div>
