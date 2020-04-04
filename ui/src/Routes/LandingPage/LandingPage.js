eb740bcfc7fdb5d2b47634e7263f5459bb5d249e
Dominik Piszczkiewicz <dominik@MacBook-Pro-Dominik.local>
2020-04-04 21:25:21 +0200

    product-page create product page

--- ui/src/Routes/LandingPage/LandingPage.js
+++ ui/src/Routes/LandingPage/LandingPage.js
@@ -5,13 +5,28 @@ import AdvantageSection from '../../components/AdvantageSection';
 import CarouselSection from '../../components/CarouselSection';
 import FarmersSection from '../../components/FarmersSection/FarmersSection';
 
+
+const dataArray = [
+  {
+    text: 'Golden apples from Jerzy in Krakow',
+    src: 'https://www.eho.eu/wp-content/uploads/2018/10/jabolko_novo.jpg"',
+  },
+  {
+    text: 'Sweet beetroots from Przemek in Gdansk',
+    src:
+      'https://www.ogrodinfo.pl/wp-content/uploads/2018/08/buraki_16_746.jpg',
+  },
+  { text: 'Tasty potatoes from Michal in Warsaw', src: 'https://n-16-1.dcs.redcdn.pl/scale/o2/tvn/web-content/m/p1/i/b432f34c5a997c8e7c806a895ecc5e25/d2788b1e-5131-4a94-910f-7e1b7d87a3bf.jpg?srcmode=0&srcx=1/20&srcy=1/1&srcw=87/100&srch=22/25&dstw=500&dsth=281&quality=80&type=1' },
+];
+
+
 export default function LandingPage() {
   return (
     <div>
       <HeroBanner />
       <AddressSearchSection />
       <AdvantageSection />
-      <CarouselSection />
+      <CarouselSection data={dataArray} title="Why is it worth it?" />
       <FarmersSection />
     </div>
   );
--- ui/src/components/CarouselSection/CarouselSection.js
+++ ui/src/components/CarouselSection/CarouselSection.js
@@ -6,22 +6,10 @@ import Carousel from '../Carousel/Carousel';
 // style
 import './style.scss';
 
-const dataArray = [
-  {
-    text: 'Golden apples from Jerzy in Krakow',
-    src: 'https://www.eho.eu/wp-content/uploads/2018/10/jabolko_novo.jpg"',
-  },
-  {
-    text: 'Sweet beetroots from Przemek in Gdansk',
-    src:
-      'https://www.ogrodinfo.pl/wp-content/uploads/2018/08/buraki_16_746.jpg',
-  },
-  { text: 'Tasty potatoes from Michal in Warsaw', src: 'https://n-16-1.dcs.redcdn.pl/scale/o2/tvn/web-content/m/p1/i/b432f34c5a997c8e7c806a895ecc5e25/d2788b1e-5131-4a94-910f-7e1b7d87a3bf.jpg?srcmode=0&srcx=1/20&srcy=1/1&srcw=87/100&srch=22/25&dstw=500&dsth=281&quality=80&type=1' },
-];
 
-export default function CarouselSection() {
+export default function CarouselSection({ data, title }) {
   function getCarouselImages() {
-    return dataArray.map(({ text, src }) => (
+    return data.map(({ text, src }) => (
       <div classNam="carousel-section__slide">
         <div className="carousel-section__image-wrapper">
 
@@ -45,7 +33,7 @@ export default function CarouselSection() {
         component="h3"
         className="carousel-section__header"
       >
-        Why is it worth it?
+        {title}
       </Typography>
       <Carousel>{getCarouselImages()}</Carousel>
     </Section>
